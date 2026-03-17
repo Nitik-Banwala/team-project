233c40dcfc94be9af79d12364306b24f8d63c8b0
rahullohchab5210 <rahullohchab5210@gmail.com>
2026-03-17 16:19:59 +0530

    circle svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
 import './index.css'
+ 
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <h1 className='text-4xl text-red-300'>jhbcikhcbj</h1>
+      
     </>
   )
 }
