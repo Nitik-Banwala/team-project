cb23b87ffea39b636a138de97d388f19b32b6f96
Vinay Kumar <vinayrsod@Vinays-MacBook-Air.local>
2026-03-18 11:58:05 +0530

    slider done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/common/Footer";
 import Hero from "./components/Hero";
 import HeroSlider from "./components/HeroSlider";
 import PupiloTopic from "./components/PupiloTopic";
+import Families from "./components/Families";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Hero />
       <HeroSlider />
       <PupiloTopic />
-      <Footer />
+      <Families />
+      {/* <Footer /> */}
     </>
   );
 }
