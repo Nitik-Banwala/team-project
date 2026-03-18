86818580ea64f238e5f204f9422febefc7fcebb6
Vinay Kumar <vinayrsod@Vinays-MacBook-Air.local>
2026-03-18 09:51:17 +0530

    image relocated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="w-full m-auto px-4">
       <div className="max-w-360 m-auto w-full">
         <img
-          src="src/assets/hero.webp"
+          src="/assets/images/webp/hero.webp"
           alt="hero"
           className="w-full h-auto object-contain sm:min-h-[400px] lg:min-h-[513px]"
         />
