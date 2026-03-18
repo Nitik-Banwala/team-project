7dc4ecae006afcf5454b6d0384835bfe7a6c85ef
k70118088-star <k70118088@gmail.com>
2026-03-18 09:31:07 +0530

    updated the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './index.css'
+import Hero from './components/Hero'
+import HeroSlider from './components/HeroSlider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-4xl text-red-300'>jhbcikhcbj</h1>
+      <h1 className='text-4xl text-red-300'>
+        <Hero />
+        <HeroSlider />
+        </h1>
     </>
   )
 }
